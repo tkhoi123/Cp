47f65b04691979fd5df11aac544a7b9b02d69c07
tkhoi123 <105137932+tkhoi123@users.noreply.github.com>
2022-09-30 12:52:41 +0700

    Add files via upload

---
+++ cf.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define el cout<<"\n"
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define F(i,l,r) for (ll i = l; i > r; --i)
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define sqr(x) ((x) * (x))
+#define fi first
+#define se second
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#define fastio cin.tie(0)->sync_with_stdio(0);cin.exceptions(cin.failbit);
+using namespace std;
+using ll = long long;
+const ll INF=1000000000;
+using ld = long double;
+using ull = unsigned long long;
+typedef std::vector<pair<int,int>> vii;
+typedef std::vector<ll> vi;
+ll t;
+ll n;
+void solve()
+{
+    cin>>n;
+}
+int main()
+{
+
+    fastio
+    t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        el;
+    }
+    return 0;
+}
