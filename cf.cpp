1e7e358b5f16ec2ff2b8d2f0555242a6b57baade
tkhoi123 <105137932+tkhoi123@users.noreply.github.com>
2022-09-30 12:55:37 +0700

    Update cf.cpp

--- cf.cpp
+++ cf.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define el cout<<"\n"
-#define f(i,l,r) for (ll i = l; i < r; ++i)
-#define F(i,l,r) for (ll i = l; i > r; --i)
+#define f(i,l,r) for (ll i = l; i < (r); ++i)
+#define F(i,l,r) for (ll i = l; i > (r); --i)
 #define pb push_back
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
