e6259dcba1cf664b014aaa4224a3da398134a9a3
tkhoi123 <105137932+tkhoi123@users.noreply.github.com>
2022-11-16 19:43:57 +0700

    Create seg.cpp

---
+++ seg.cpp
@@ -0,0 +1,42 @@
+ll n,Node[maxn*4],a[maxn],q;
+void build(ll id,ll l,ll r)
+{
+    if(l==r)
+    {
+        Node[id]=a[l];
+        return;
+    }
+    ll mid=(l+r)>>1;
+    build(id*2,l,mid);
+    build(id*2+1,mid+1,r);
+    Node[id]=Node[id*2]+Node[id*2+1];
+}
+ll get(ll id,ll l,ll r,ll u,ll v)
+{
+    if(l>v||r<u)
+    {
+        return 0;
+    }
+    if(l>=u&&r<=v)
+    {
+        return Node[id];
+    }
+    ll mid=(l+r)>>1;
+    return get(id*2,l,mid,u,v)+get(id*2+1,mid+1,r,u,v);
+}
+void update(ll id,ll l,ll r,ll u,ll v,ll val)
+{
+    if(l>v||r<u)
+    {
+        return ;
+    }
+    if(l==r)
+    {
+        Node[id]+=val;
+        return;
+    }
+    ll mid=(l+r)>>1;
+    update(id*2,l,mid,u,v,val);
+    update(id*2+1,mid+1,r,u,v,val);
+    Node[id]=Node[id*2]+Node[id*2+1];
+}
