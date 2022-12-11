4da939ba2e6164690d6fc2abc786b83bf618c3ce
tkhoi123 <105137932+tkhoi123@users.noreply.github.com>
2022-12-11 22:32:30 +0700

    Update codeforces.cpp

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
 #define el cout<<"\n"
-#define f(i,l,r) for (ll i = l; i < (r); ++i)
-#define F(i,l,r) for (ll i = l; i > (r); --i)
 #define pb push_back
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define sqr(x) ((x) * (x))
-#define fi first
-#define se second
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 using namespace std;
@@ -18,6 +14,7 @@ using ull = unsigned long long;
 typedef vector<pair<ll,ll>> vii;
 typedef vector<ll> vi;
 typedef pair<ll,ll> pii;
+const ll maxn=100001;
 ll t;
 ll n;
 void solve()
@@ -27,7 +24,7 @@ void solve()
 int main()
 {
 
-    cin.tie(0)->sync_with_stdio(0);cin.exceptions(cin.failbit);
+    cin.tie(0)->sync_with_stdio(0);
     t=1;
     cin>>t;
     while(t--)
