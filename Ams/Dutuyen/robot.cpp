dee1f8f60113d8abeeb04efe475f89a3279b1f8b
tkhoi123 <105137932+tkhoi123@users.noreply.github.com>
2022-12-31 22:58:52 +0700

    Create robot.cpp

---
+++ Ams/Dutuyen/robot.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define el cout<<"\n"
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define sqr(x) ((x) * (x))
+#define fi first
+#define se second
+#define NAME "temp"
+using namespace std;
+using ll = long long;
+using ld = long double;
+const ll INF=1000000000;
+typedef vector<pair<ll,ll>> vii;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pii;
+const ll maxn=100001;
+ll n;
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    //freopen(NAME".INP","r",stdin);freopen(NAME".OUT","w",stdout);
+    cin>>n;
+    for(ll i=floor(sqrt(n));;--i)
+    {
+        if(n%i==0)
+        {
+            cout<<n/i+i-2;exit(0);
+        }
+    }
+    return 0;
+}
